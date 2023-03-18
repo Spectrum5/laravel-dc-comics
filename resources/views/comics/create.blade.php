936cc2fa0e8ff62ea26b5d1d02f31dcfbe33b42a
Spectrum5 <toskusergio@gmail.com>
2023-03-18 21:56:32 +0100

    Aggiungo validazione e form con gestione errori

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -5,33 +5,67 @@
         @csrf
         <div class="mb-3">
             <label for="title" class="form-label">Title</label>
-            <input type="text" class="form-control" name="title" id="title" placeholder="Inserire il titolo">
+            <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" id="title"
+                value="{{ old('title') }}" placeholder="Inserire il titolo">
+            @error('title')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="image" class="form-label">Thumb</label>
-            <input type="text" class="form-control" name="thumb" id="thumb"
-                placeholder="Inserire la URL dell'immagine">
+            <input type="text" class="form-control @error('thumb') is-invalid @enderror" name="thumb" id="thumb"
+                value="{{ old('thumb') }}" placeholder="Inserire la URL dell'immagine">
+            @error('thumb')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="price" class="form-label">Price</label>
-            <input type="text" class="form-control" name="price" id="price" placeholder="prezzo">
+            <input type="text" class="form-control @error('price') is-invalid @enderror" name="price" id="price"
+                value="{{ old('price') }}" placeholder="prezzo">
+            @error('price')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="type" class="form-label">Type</label>
-            <input type="text" class="form-control" name="type" id="type" placeholder="tipo di fumettto">
+            <input type="text" class="form-control @error('type') is-invalid @enderror " name="type" id="type"
+                value="{{ old('type') }}" placeholder="tipo di fumettto">
+            @error('type')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="series" class="form-label">Series</label>
-            <input type="text" class="form-control" name="series" id="series" placeholder="Inserire la serie">
+            <input type="text" class="form-control @error('series') is-invalid @enderror" name="series" id="series"
+                value="{{ old('series') }}" placeholder="Inserire la serie">
+            @error('series')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="sale_date" class="form-label">Sale date</label>
-            <input type="text" class="form-control" name="sale_date" id="sale_date" placeholder="data di vendita">
-        </div>
-        <div class="mb-3">
-            <label for="description" class="form-label">Description</label>
-            <textarea class="form-control" name="description" id="description" rows="3"></textarea>
-        </div>
-        <button type="submit" class="btn btn-primary mb-5">Send</button>
+            <input type="text" class="form-control @error('sale_date') is-invalid @enderror" name="sale_date"
+                id="sale_date" value="{{ old('sale_date') }}" placeholder="data di vendita">
+            @error('sale_date')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+            <div class="mb-3">
+                <label for="description" class="form-label">Description</label>
+                <textarea class="form-control" name="description" id="description" rows="3">{{ old('description') }}</textarea>
+            </div>
+            <button type="submit" class="btn btn-primary mb-5">Send</button>
     </form>
 @endsection
